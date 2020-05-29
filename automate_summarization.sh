78a78965cdad4dca9709e5ba95c21ccbefb9ed83
Parth <parth.mehta126@gmail.com>
2020-05-29 12:58:01 +0530

    move all generated files under a temp directory

--- automate_summarization.sh
+++ automate_summarization.sh
@@ -3,7 +3,7 @@ while getopts ":i:o:p:e:d:" opt; do
     i) in_dir="$OPTARG"
     ;;
     o) out_dir="$OPTARG"
-    ;;    
+    ;;
     p) base_dir="$OPTARG"
     ;;
     e) enc_step="$OPTARG"
@@ -14,22 +14,33 @@ while getopts ":i:o:p:e:d:" opt; do
     ;;
   esac
 done
-#echo "ppp" $in_dir $out_dir $base_dir
+
 in_dir=$(echo $in_dir | sed 's/\/$//g')
 out_dir=$(echo $out_dir | sed 's/\/$//g')
 base_dir=$(echo $base_dir | sed 's/\/$//g')
 export CLASSPATH=$base_dir/stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar
+
+# Remove the decoded folder
+rm -r $base_dir/experiments/temp/pretrained/decode*
+
+# Remove the temp folder (sentence_per_line, transcripts_tokenized, finished_files)
+rm -r $base_dir/experiments/temp
+
 python $base_dir/automate_preprocessing.py $in_dir
-rm -r $base_dir/models/pretrained/decode*
-python $base_dir/run_summarization.py --mode=decode --data_path=$base_dir/experiments/finished_files/chunked/test_* --vocab_path=$base_dir/dataset/processed/finished_files/vocab --log_root=$base_dir/models/ --exp_name=pretrained --max_enc_steps=$enc_step --max_dec_steps=$dec_step --coverage=1 --single_pass=1
+
+python $base_dir/run_summarization.py --mode=decode --data_path=$base_dir/experiments/temp/finished_files/chunked/test_* --vocab_path=$base_dir/dataset/processed/finished_files/vocab --log_root=$base_dir/experiments/temp --exp_name=pretrained --max_enc_steps=$enc_step --max_dec_steps=$dec_step --coverage=1 --single_pass=1
+
 mkdir -p $out_dir/text
+
 mkdir -p $out_dir/attention_scores
-mv $base_dir/models/pretrained/decode* $base_dir/models/pretrained/decode
-for i in $base_dir/models/pretrained/decode/reference/*;
+
+mv $base_dir/experiments/temp/pretrained/decode* $base_dir/experiments/temp/pretrained/decode
+
+for i in $base_dir/experiments/temp/pretrained/decode/reference/*;
 do
 	j=$(echo $i | rev | cut -d '/' -f 1 | rev | cut -d '_' -f 1);
 	target=$(cat $i | sed 's/ //g' | cut -d '.' -f 1);
 	cat $base_dir'/models/pretrained/decode/decoded/'$j'_decoded.txt' | sed -zE 's/[[:space:]]([,.?!])/\1/g' > $out_dir/text/$target'_'$enc_step'_'$dec_step.txt
-	mv $base_dir'/models/pretrained/decode/attention/'$j'_attn_vis_data.json' $out_dir/attention_scores/$target'_'$enc_step'_'$dec_step'.attn_vis_data.json'
+	mv $base_dir'/experiments/temp/pretrained/decode/attention/'$j'_attn_vis_data.json' $out_dir/attention_scores/$target'_'$enc_step'_'$dec_step'.attn_vis_data.json'
 done
 echo "Summarization Successfully completed"
